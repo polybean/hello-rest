75c37b7e7b596c160b96f5aea2225fd2e32e166c
Poly Bean <polybean@gmail.com>
2019-06-30 19:55:19 +0800

    Create inital empty Spring Boot project

---
+++ src/test/java/com/example/issueservice/IssueServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.issueservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IssueServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
