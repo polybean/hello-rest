2ef5b7a558bc3e67e23bfe6ee693c6a9e7fa406a
Poly Bean <polybean@gmail.com>
2019-07-01 12:50:34 +0800

    Add structured query string handling

--- src/main/java/com/example/issueservice/controller/Issues.java
+++ src/main/java/com/example/issueservice/controller/Issues.java
@@ -8,11 +8,41 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "/issues")
 public class Issues {
+    static class Params {
+        String author;
+        int type;
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public int getType() {
+            return type;
+        }
+    }
+
     @GetMapping
     public Map<String, String> index(@RequestParam String author, @RequestParam int type) {
         Map<String, String> body = new HashMap<>();
         body.put("author", author);
         body.put("type", String.valueOf(type));
+
+        return body;
+    }
+
+    @GetMapping(path = "params")
+    public Map<String, String> indexUsingParamStructure(Params params) {
+        Map<String, String> body = new HashMap<>();
+        body.put("author", params.author);
+        body.put("type", String.valueOf(params.type));
 
         return body;
     }
