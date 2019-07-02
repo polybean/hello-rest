e9b98677ace5315341d6df96295d81b1f7f34d9d
Poly Bean <polybean@gmail.com>
2019-07-02 17:38:37 +0800

    Create sample links to the hypermedia testing resource

--- src/main/java/com/example/issueservice/controller/Hypermedia.java
+++ src/main/java/com/example/issueservice/controller/Hypermedia.java
@@ -1,15 +1,22 @@
 package com.example.issueservice.controller;
 
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.BasicLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/hypermedia")
 public class Hypermedia {
     @GetMapping
     public Resource<String> index() {
-        return new Resource<>("This is just a test");
+        return new Resource<>("This is just a test",
+                BasicLinkBuilder.linkToCurrentMapping().withSelfRel(),
+                BasicLinkBuilder.linkToCurrentMapping().slash("issues").withRel("issues"),
+                linkTo(methodOn(Issues.class).index()).withRel("issues2"));
     }
 }
